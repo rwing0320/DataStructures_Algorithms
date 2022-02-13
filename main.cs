7091e82613c57a840670d02ae7fe31cd3555ef4e
Ryan W Arena <ryan_645@outlook.com>
2022-02-13 15:34:05 +0000

    Initial commit

--- main.cs
+++ main.cs
@@ -5,24 +5,8 @@ class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
 
-    string[] nemo = {"nemo"};
-
-    FindNemo(nemo);
-
+   
   }
 
-  public static void FindNemo(string[] array){
-      
-      DateTime t1 = DateTime.Now;
-      
-      for(int i = 0; i < array.Length; i++){
-        if(array[i] == "nemo"){
-          Console.WriteLine("FOUND NEMO");
-        }
-      }
-
-      DateTime t2 = DateTime.Now;
-
-      Console.WriteLine("Call to find Nemo took " + (t2 - t1).TotalMilliseconds + " milliseconds");
-  }
+ 
 }
