7e676349b53001fe8798a6587bfb6c1f74ff9ec8
Ryan W Arena <ryan_645@outlook.com>
2022-02-13 16:10:56 +0000

    Test Example

--- main.cs
+++ main.cs
@@ -5,8 +5,20 @@ class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
 
-   
+    
   }
 
+  //Example for interview quesiton
+
+  //Give 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain common items
+
+  //For example:
+  //string[] array1 = {"a", "b", "c", "x"};
+  //string[] array2 = {"z", "y", "i"};
+  //should return false.
+  //-----------
+  //string[] array1 = {"a", "b", "c", "x"};
+  //string[] array2 = {"z", "y", "x"};
+  //should return true.
  
 }
